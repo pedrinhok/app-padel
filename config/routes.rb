d5e6e8a92c1fcd5f32b8543295dded6c149f5f79
Pedro Kayser <pedrokayser00@gmail.com>
2017-01-12 17:14:06 -0200

    Boostrap Template Cyborg

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+	root "sessions#new"
+
+  get "sign_in" => "sessions#new"
+  post "sessions" => "sessions#create", as: :sessions
+  delete "sign_out" => "sessions#destroy", as: :sign_out
+
   resources :users
 
 end
