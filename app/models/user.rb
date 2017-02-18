4b9935bece96d1c373952fa13df2e878bb9644f1
Pedro Kayser <pedrokayser00@gmail.com>
2017-02-17 22:34:34 -0200

    Bug Fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
 
 	validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
 	validates :email, presence: true, format: { with: /\A\S+@.+\.\S+\z/ }, uniqueness: true
-	validates :password, length: { minimum: 3 }
 
 	has_secure_password
 
