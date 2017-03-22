099605c52368169dac317249f3f40ca44b50a302
Pedro Kayser <pedrokayser00@gmail.com>
2017-03-22 08:23:21 -0300

    Sessions controller rspecs

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,4 +2,58 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
+	describe "GET #new" do
+
+		context "when user is authenticated" do
+
+			it "redirects to users#index" do
+				user = FactoryGirl.create(:user)
+				post :create, { session: { email: user.email, password: user.password } }
+				get :new
+				expect(response).to redirect_to(users_path)
+			end
+
+		end
+
+	end
+
+	describe "POST #create" do
+
+		context "with invalid params" do
+
+			it "return error message" do
+				user = FactoryGirl.create(:user)
+				post :create, { session: { email: user.email, password: "#{user.password}." } }
+				expect(flash[:danger]).to_not be_nil
+			end
+
+		end
+
+		context "with valid params" do
+
+			it "authenticates user" do
+				user = FactoryGirl.create(:user)
+				post :create, { session: { email: user.email, password: user.password } }
+				expect(session[:user_id]).to eq(user.id)
+			end
+
+		end
+
+	end
+
+	describe "DELETE #destroy" do
+
+		context "when user is authenticated" do
+
+			it "destroys user session" do
+				user = FactoryGirl.create(:user)
+				post :create, { session: { email: user.email, password: user.password } }
+				delete :destroy
+				expect(session[:user_id]).to be_nil
+			end
+
+		end
+
+	end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,8 +15,6 @@ RSpec.describe User, type: :model do
 
 	it { should validate_uniqueness_of(:email) }
 
-	it { should validate_length_of(:password).is_at_least(3) }
-
 	it { should validate_confirmation_of(:password) }
 
 	it { should have_secure_password }
